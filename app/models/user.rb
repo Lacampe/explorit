7281dab4a45beaa1393cfb7652b324a6dbd13f44
Romain Campech <romain.campech@city.ac.uk>
2018-02-25 21:16:42 +0100

    Github omniauth working sexy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,38 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: %i[github]
 
+  # Github omniauth
   def self.find_for_github_auth(auth)
-    p auth.credentials.token
     p auth.extra.raw_info.followers
     p auth.extra.raw_info.following
     p auth.extra.raw_info.location
     p auth.extra.raw_info.public_repos
-    p auth.info.email
-    p auth.info.image
-    p auth.info.name
-    p auth.info.nickname
-    p auth.provider
-    p auth.uid
+
+    user_params = {
+      provider: auth.provider,
+      uid: auth.uid,
+      token: auth.credentials.token,
+      email: auth.info.email,
+      name: auth.info.name,
+      nickname: auth.info.nickname,
+      location: auth.extra.raw_info.location,
+      github_image_url: auth.info.image,
+      github_followers: auth.extra.raw_info.followers,
+      github_following: auth.extra.raw_info.following,
+      github_public_repos: auth.extra.raw_info.public_repos
+    }
+
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.find_by(email: auth.info.email)
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]
+      user.save
+    end
+
+    return user
   end
+
 end
