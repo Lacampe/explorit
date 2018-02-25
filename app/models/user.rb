8a6cce1786ceec7da19ba4f32d38060fe3675e78
Romain Campech <romain.campech@city.ac.uk>
2018-02-25 20:46:06 +0100

    Set up Guthub OmniAuth - WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: %i[github]
+
+  def self.find_for_github_auth(auth)
+    p auth.credentials.token
+    p auth.extra.raw_info.followers
+    p auth.extra.raw_info.following
+    p auth.extra.raw_info.location
+    p auth.extra.raw_info.public_repos
+    p auth.info.email
+    p auth.info.image
+    p auth.info.name
+    p auth.info.nickname
+    p auth.provider
+    p auth.uid
+  end
 end
