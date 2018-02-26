6ce36abbd6f9597000e8dce95d647b299a29aaff
Romain Campech <romain.campech@city.ac.uk>
2018-02-26 11:17:08 +0100

    Set up react

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '*path', to: 'pages#home'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
